a7869926e710151494d227f4119ff184edb08ed5
uctuviun <gusboehling@gmail.com>
2018-11-25 15:36:51 -0500

    add sfx for herospells, dialogue, switch herospell key locations

---
+++ Assets/scripts/audio_canceler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_canceler : MonoBehaviour {
+
+	AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+		audiosource = GetComponent<AudioSource>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(PlayerPrefs.GetInt("soundOn") == 0){
+			audiosource.enabled = false;
+		}
+	}
+}
--- Assets/scripts/dialogue_destruct.cs
+++ Assets/scripts/dialogue_destruct.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class dialogue_destruct : MonoBehaviour {
 
 	public int duration;
+	AudioSource audiosource;
 
 	// Use this for initialization
 	void Start () {
+		audiosource = GetComponent<AudioSource>();
 //		Destroy(gameObject, duration);
 	}
 
 	// Update is called once per frame
 	void Update () 		{
+		if(PlayerPrefs.GetInt("soundOn") == 0){
+			audiosource.enabled = false;
+		}
 		if(Input.GetKeyDown("space")){
 				Destroy(gameObject);
 			}
